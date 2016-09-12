0cfe928da7a6c0429b31c35f1d9a40aed288523c
dogrdon <drew.s.gordon@gmail.com>
2016-09-11 20:49:39 -0400

    making the objects
    still need to deal with lots

--- scripts/inkworks_json.py
+++ scripts/inkworks_json.py
@@ -9,7 +9,7 @@
 
 import os, sys
 import argparse
-import csv
+import csv, json
 import requests
 import config
 
@@ -21,33 +21,63 @@ CA_RELATIONSHIP_IDS = {"lot_id": 134,
                    "storage_id": 'NA'}
 LOT_ID = 305 #not sure if this is how lot relationships are assigned.
 NEW_ITEM_TYPE_ID = 435
+ENTITY_STORE = '../data/inkworks/new_entities.csv'
+ENTITIES = {row['entity']:row['catalog_id'] for row in csv.DictReader(open(ENTITY_STORE, 'r'), delimiter='\t')}
+clients = ['client_1', 'client_2', 'client_3', 'client_4']
+designers = ['designer_1', 'designer_2', 'designer_3']
+photographers = ['photographer_1', 'photographer_2']
+source_artists = ['source_artist_1', 'source_artist_2', 'source_artist_3', 'source_artist_4']
 
-def handle_entity(entities):
-    pass
+
+def handle_entities(entities, relation_type):
+    return [{'entity_id':ENTITIES[entity], 'type_id':relation_type} for entity in entities if entity in ENTITIES.keys()]
+
 
 def make_item(row):
-    item = {'intrinsic_fields':{}, 'preferred_labels':[], 'attributes':{
-                                                                        'notes':[],
-                                                                        'titleType':[],
-                                                                        'dataSet':[],
-                                                                        'measurements_field':[]
-    }, 'related':{}}
+    keys = row.keys()
+    item = {'intrinsic_fields':{}, 
+            'preferred_labels':[], 
+            'attributes':{
+                'notes':[],
+                'titleType':[],
+                'dateSet':[],
+                'measurements_field':[],
+                'rightsSet':[],
+                'format':[{"locale" : "en_US", "name" : "prints"}]
+            }, 
+            'related':{"ca_entities" : []}}
     image = row['filename']
     idno = image.split('.')[0] #for matching media on upload, give it the filename id
     item['intrinsic_fields']['idno'] = idno
     item['intrinsic_fields']['type_id'] = NEW_ITEM_TYPE_ID
     item['preferred_labels'].append({"locale" : "en_US", "name" : row['title']})
-    item['attributes']['notes'].append({"locale" : "en_US", "name" : row['notes']})
     item['attributes']['titleType'].append({"locale" : "en_US", "name" : row['title']})
-    item['attributes']['dateSet'].append({"locale" : "en_US", "name" : row['date']})
-    item['attributes']['measurements_field'].append({"locale" : "en_US", "name" : row['size']})
-    # RIGHTS?
+    if row['notes'] != '':
+        item['attributes']['notes'].append({"locale" : "en_US", "name" : row['notes']})
+    if row['date'] != '':
+        item['attributes']['dateSet'].append({"locale" : "en_US", "name" : row['date']})
+    if row['size'] != '':
+        item['attributes']['measurements_field'].append({"locale" : "en_US", "name" : row['size']})
+    if row['rights'] != '':
+        item['attributes']['rightsSet'].append({"locale" : "en_US", "name" : row['rights']})
+
+    row_clients = ([row[i] for i in clients if i != ''], CA_RELATIONSHIP_IDS['client_id'])
+    row_designers = ([row[i] for i in designers if i != ''], CA_RELATIONSHIP_IDS['designer_id'])
+    row_photographers = ([row[i] for i in photographers if i != ''], CA_RELATIONSHIP_IDS['photographer_id'])
+    row_source_artists = ([row[i] for i in source_artists if i != ''], CA_RELATIONSHIP_IDS['sourceartist_id'])
+
+    relateds = [row_clients, row_designers, row_photographers, row_source_artists]
+
+    for r in relateds:
+        if r[0] != []:
+            item['related']['ca_entities'].extend(handle_entities(r[0], r[1]))
+    return item
 
 def main(arguments):
 
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('infile', help="Input file", type=argparse.FileType('r'))
+    parser.add_argument('-i', '--infile', help="Input file", type=argparse.FileType('r'))
     parser.add_argument('-o', '--outfile', help="Output file",
                         default=sys.stdout, type=argparse.FileType('w'))
 
@@ -56,18 +86,18 @@ def main(arguments):
     outfile = args.outfile
 
     with infile:
-    	reader = csv.DictReader(infile)
+    	reader = csv.DictReader(infile, delimiter='\t')
     	header = reader.fieldnames
 
         items = []
 
         for row in reader:
-
+            items.append(make_item(row))
 
         data = json.dumps(items, indent=4)
 
-        with open(outfile, 'w') as outf:
-            outf.write(data)
+    with outfile:
+        outfile.write(data)
 
 
 if __name__ == '__main__':
